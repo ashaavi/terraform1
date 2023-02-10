ff53634c11aef9d149dcc9e2f21bb18935dea0fa
ashaavi <116795694+ashaavi@users.noreply.github.com>
2023-02-10 03:53:11 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "Instance-01" {
+  ami                     = "ami-01a4f99c4ac11b03c"
+  instance_type           = "t2.micro"
+  key_name                = "newkey"
+   tags = {
+    Name = "HelloWorld"
+  }
+}
+Footer
+© 2023 GitHub, Inc.
